11c8e487d0c62e3b99d642e84ba47e3a64936bfb
KaliforniaKid <37274112+KaliforniaKid@users.noreply.github.com>
2018-03-11 15:12:47 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+
+namespace Bot1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+       => new Program().StartAsync().GetAwaiter().GetResult();
+    
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+           
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "NDIxMjYzNDM4OTU5MDgzNTQw.DYS2lQ.-i37OIcFrRp1dSiJbV0NW2UYmYU");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            await Task.Delay(-1);
+        }
+    }
+}
